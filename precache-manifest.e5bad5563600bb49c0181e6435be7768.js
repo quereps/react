a6f77a02e564eff4595f9d4fae3afe6f34bfd670
quereps <quereps@gmail.com>
2020-02-01 17:09:49 +0000

    first commit

---
+++ precache-manifest.e5bad5563600bb49c0181e6435be7768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fe06120d51fc0d1124b8f6612213ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c5909688428a6447f08",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.33af256b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5909688428a6447f08",
+    "url": "/static/js/main.93d3545b.chunk.js"
+  },
+  {
+    "revision": "2c2425d94d2675c7c0c8",
+    "url": "/static/js/runtime-main.0018e94b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
